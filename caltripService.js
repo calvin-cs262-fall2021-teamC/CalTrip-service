570bdfbc2dc2c39369bb0ee61adbf3787cd9b778
YkSlavik <81549171+YkSlavik@users.noreply.github.com>
2021-12-11 05:39:51 -0500

    Update caltripService.js

--- caltripService.js
+++ caltripService.js
@@ -155,9 +155,10 @@ function createEvent(req, res, next) {
 }
 
 function findUser(req, res, next) {
-  db.one('SELECT * FROM TheUser WHERE emailAddress=${emailAddress} AND password=${password}', req.params)
+  db.one('SELECT * FROM TheUser WHERE emailAddress=${emailAddress} AND password=${password}', req.body)
     .then(data => {
       console.log(data);
+      
       returnDataOr404(res, data);
     })
     .catch(err => {
